0ba9f094da909f65927f2f5b3ad559873d1b8c21
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2015-08-08 20:46:08 +0000

    Bootstrapped the 2.0 version of the plex bundle.

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,99 @@
+import urllib, urllib2, base64
+
+# Const
+VERSION  = '0.0.1'
+ART      = 'art-default.png'
+ICON     = 'icon-default.png'
+PREFIX   = '/video/oz-eon'
+HOST     = 'core.oz.com'
+WAYPOINT = 'https://'+HOST+'/'
+AGENT    = 'OZ-Eon.bundle '+VERSION+' (Plex Media Server)'
+
+
+####################################################################################################
+def Start():
+
+  Log('Starting OZ Eon v%s', VERSION)
+  ObjectContainer.title1 = 'OZ Eon'
+
+  if 'access_token' not in Dict:
+    Authenticate()
+
+
+####################################################################################################
+def Request(Path):
+  url = WAYPOINT + Path
+  request = {}
+  headers = {}
+  headers['User-Agent'] = AGENT
+  headers["Authorization"] = "Bearer " + Dict['access_token']
+  body = urllib2.urlopen(urllib2.Request(url, None, headers)).read()
+  response = JSON.ObjectFromString(body)
+
+  return response
+
+
+####################################################################################################
+@handler(PREFIX, 'OZ Eon', art=ART, thumb=ICON)
+def MainMenu():
+
+  oc = ObjectContainer()
+
+  oc.add(DirectoryObject(key = Callback(DiscoverMenu),                 title = L('Discover')))
+  oc.add(DirectoryObject(key = Callback(ChannelMenu),                  title = L('Channels')))
+  oc.add(InputDirectoryObject(key = Callback(Search, title = L('Search')), prompt = L('Search'), title = L('Search')))
+
+  return oc
+
+
+####################################################################################################
+@handler(PREFIX + '/discover')
+def DiscoverMenu():
+
+  oc = ObjectContainer(title2 = L('Discover'))
+
+  if len(oc) < 1:
+    return NoContentFound(oc, title)
+
+  return oc
+
+
+####################################################################################################
+@route(PREFIX + '/channels')
+def ChannelMenu():
+
+  oc = ObjectContainer(title2 = L('Channels'))
+
+  channels = Request('users/me/channels')
+
+  if len(oc) < 1:
+    return NoContentFound(oc, title)
+
+  return oc
+
+
+####################################################################################################
+@route(PREFIX + '/collections', channel = object)
+def ChannelMenu():
+
+  oc = ObjectContainer(title2 = L(channel['title']))
+
+
+  return oc
+
+####################################################################################################
+@route(PREFIX + '/search')
+def Search(query = ''):
+
+  oc = ObjectContainer(title2 = L('Search results'))
+
+  if len(oc) < 1:
+    return NoContentFound(oc, title)
+
+  return oc
+
+####################################################################################################
+def NoContentFound(oc, title):
+  oc.header  = title
+  oc.message = L('No content found')
+  return oc
---
+++ README.md
@@ -0,0 +1,17 @@
+OZ-Eon.bundle
+=============
+
+OZ Eon TV bundle for Plex Media Server
+
+ - Works with PlexConnect's Apple TV player
+
+Installation
+------------
+
+1. Copy and paste **OZ-Eon.bundle** folder to your **%AppData%/Plex Media Server/Plug-ins** folder.
+2. Open OZ and enjoy, please report bugs if you encounter one.
+
+Known bugs
+----------
+
+* Does not work in Web Player because of AES encryption
